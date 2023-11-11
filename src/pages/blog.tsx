ff27153a2dec9ca5f6eced2a8839b541bfcfb57b
chae-chae <abed_nadir@naver.com>
2023-11-11 14:39:55 +0900

    add gatsby-source-filesystem

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { PageProps, graphql } from "gatsby";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+  console.log(data);
   return (
     <Layout title="blog">
       <p>recent news</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
